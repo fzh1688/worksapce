08670752563b545d78a9c6ba09b3ca4d1bcad927
fzh1688 <1547408844@qq.com>
2020-07-13 19:50:24 +0800

    更改parametrize里的参数

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -4,7 +4,7 @@ import yaml
 from pythoncode.calc import Calculator
 calc = Calculator()
 with open("../data/calcdata.yaml") as fr:
-    data = yaml.safe_load(fr)
+ data = yaml.safe_load(fr)
 add_mydatas = list(data["add"].values())
 add_myids = list(data["add"].keys())
 sub_mydatas = list(data["sub"].values())
@@ -31,19 +31,19 @@ class TestCalc:
 
 #减法
     @pytest.mark.sub
-    @pytest.mark.parametrize(("a, b, result"), add_mydatas, ids=add_myids)
+    @pytest.mark.parametrize(("a, b, result"), sub_mydatas, ids=sub_myids)
     def test_sub(self,a,b,result):
         assert result == calc.sub(a, b)
 
 #乘法
     @pytest.mark.mul
-    @pytest.mark.parametrize(("a, b, result"), add_mydatas, ids=add_myids)
+    @pytest.mark.parametrize(("a, b, result"), mul_mydatas, ids=mul_myids)
     def test_mul(self,a,b,result):
         assert result == calc.mul(a, b)
 
 #除法
     @pytest.mark.div
-    @pytest.mark.parametrize(("a, b, result"), add_mydatas, ids=add_myids)
+    @pytest.mark.parametrize(("a, b, result"), div_mydatas, ids=div_myids)
     def test_div(self,a,b,result):
         assert result == calc.div(a, b)
 
